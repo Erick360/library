74f62928a7ede37e39aa78ccdbc83bbe494bdf30
eri <eri360roperez@gmail.com>
2024-04-19 15:57:16 -0600

    update

---
+++ library-api/routes/api.php
@@ -0,0 +1,16 @@
+<?php
+use App\Http\Controllers\BookController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/books', [BookController::class, 'store']);
+Route::get('/books', [BookController::class, 'index']);
+Route::put('/books/{id}', [BookController::class, 'update']);
+Route::delete('/books/{id}', [BookController::class, 'destroy']);
+
+/*Route::get('/user', function (Request $request) {
+ 
+    return $request->user();
+})->middleware('auth:sanctum');
+
+*/
